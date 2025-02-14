586d76973a83fa40bdee1eb60364a123b12c4a1d
sporak577 <sophie.porak@ucsf.edu>
2025-02-13 21:22:59 -0800

    tests finished

--- test/test_align.py
+++ test/test_align.py
@@ -12,10 +12,16 @@ def test_nw_alignment():
     Use the BLOSUM62 matrix and a gap open penalty
     of -10 and a gap extension penalty of -1.
     """
+    #in the read_fasta, the return order is seq, header. so header is our throwaway variable, we only care for the sequence
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
-    pass
-    
+
+    nw = NeedlemanWunsch("BLOSUM62.txt", gap_open= -10, gap_extend= -1)
+    nw.align(seq1, seq2)
+
+    #check if matrix is correctly initialized 
+    assert nw._align_matrix.shape == (len(seq1) + 1, len(seq2) + 1)
+
 
 def test_nw_backtrace():
     """
@@ -27,7 +33,17 @@ def test_nw_backtrace():
     """
     seq3, _ = read_fasta("./data/test_seq3.fa")
     seq4, _ = read_fasta("./data/test_seq4.fa")
-    pass
+    
+    nw = NeedlemanWunsch("BLOSUM62.txt", gap_open= -10, gap_extend= -1)
+    score, aligned_seqA, aligned_seqB = nw.align(seq3, seq4)
+
+    expected_aligned_seqA = "MAVHQLIRRP"
+    expected_aligned_seqB = "M---QLIRHP"
+
+    assert aligned_seqA == expected_aligned_seqA
+    assert aligned_seqB == expected_aligned_seqB
+
+
 
 
 
